cd575eed2c3b3ef0559a06076a4b0310f09a6da2
Matthias Felleisen <matthias@ccs.neu.edu>
2017-06-29 14:01:29 -0400

    make Functions1 a truly interactive language

--- RacketSchool/Functions1/lang.rkt
+++ RacketSchool/Functions1/lang.rkt
@@ -1,14 +1,33 @@
 #lang racket
 
-(provide (rename-out [top-interaction #%top-interaction]
-                     [module-begin #%module-begin]))
+(provide
+ (rename-out
+  [top-interaction #%top-interaction]
+  [module-begin    #%module-begin]))
 
-(require "../mystery.rkt" (for-syntax syntax/parse) redex/reduction-semantics)
+;; ---------------------------------------------------------------------------------------------------
+;; dependencies
+
+(require "../mystery.rkt"
+         "../mystery-functions.rkt"
+         (for-syntax "lang/reader.rkt")
+         (for-syntax syntax/parse)
+         redex/reduction-semantics)
+
+
+;; ---------------------------------------------------------------------------------------------------
+;; implmenetation
 
 (define-syntax (top-interaction stx)
-  (syntax-parse stx
-    [(_ . e) #'(#%top-interaction . (run functions1 (prog e)))]))
+  (syntax-case stx ()
+    [(_ . e)
+     #`(#%top-interaction . (run functions1 (prog ,@ #,dw e)))]))
 
 (define-syntax (module-begin stx)
   (syntax-parse stx
-    [(_ e ...) #'(#%module-begin (run functions1 (prog e ...)))]))
+    [(_ id:id e ...) ;; the id is added by the reader (see reader.rkt) 
+     #`(#%module-begin
+        (define id
+          (filter (redex-match? func-lang-1 (defun (x_1 x_2) e_1)) (term (e ...))))
+        (run functions1 (prog e ...)))]))
+

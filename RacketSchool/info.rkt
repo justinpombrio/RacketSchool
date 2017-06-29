36d816d8f3dc1b36e39f6b858398be6c16e6b2ce
Matthias Felleisen <matthias@ccs.neu.edu>
2017-06-29 14:36:49 -0400

    starting to package

---
+++ RacketSchool/info.rkt
@@ -0,0 +1,11 @@
+#lang info
+
+(define collection 'multi)
+
+; (define deps '("htdp-lib" "htdp-doc"))
+; (define implies '("htdp-lib" "htdp-doc"))
+
+(define pkg-desc
+  "Mystery Languages for the Racket Summer School of Semantics and Languages")
+
+(define pkg-authors '(justin matthias shriram))

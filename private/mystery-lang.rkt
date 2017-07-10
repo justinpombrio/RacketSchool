7b3d293b6b2c972492d9698005618d8149974064
Matthew Flatt <mflatt@racket-lang.org>
2017-07-10 05:45:11 -0600

    collapse `define-language` and  define-language-set`

--- private/mystery-lang.rkt
+++ private/mystery-lang.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(provide define-language define-language-set)
+(provide define-language)
 
 ;; Defines a language like `Records1` in terms of a Redex reduction
 ;; like `records1` --- given the grammar and a pattern for recognizing
@@ -8,101 +8,48 @@
 
 (define-syntax-rule (define-language
                       #:module-name lang-module-name
-                      #:reductions reductions
+                      #:reductions (reductions ...)
                       #:grammar grammar-id
                       #:defn-pattern defn-pattern)
-  (...
-   (begin
-     
-     (provide
-      (rename-out
-       [top-interaction #%top-interaction]
-       [module-begin    #%module-begin]))
+  (begin
+    
+    (provide
+     (rename-out
+      [top-interaction #%top-interaction]
+      [module-begin    #%module-begin]))
 
-     ;; ---------------------------------------------------------------------------------------------------
-     ;; dependencies
+    ;; ---------------------------------------------------------------------------------------------------
+    ;; dependencies
 
-     (require RacketSchool/private/mystery
-              (for-syntax racket/base
-                          syntax/parse)
-              redex/reduction-semantics)
+    (require RacketSchool/private/mystery
+             (for-syntax racket/base
+                         syntax/parse)
+             redex/reduction-semantics)
 
-     ;; ---------------------------------------------------------------------------------------------------
-     ;; implementation
+    ;; ---------------------------------------------------------------------------------------------------
+    ;; implementation
 
-     (define-syntax (top-interaction stx)
-       (syntax-case stx ()
-         [(_ . e)
-          #`(#%top-interaction . (run reductions (prog ,@definitions e)))]))
+    (define-syntax (top-interaction stx)
+      (syntax-case stx ()
+        [(_ . e)
+         #`(#%top-interaction . (run-all reductions ... (prog ,@definitions e)))]))
 
-     (define-syntax (module-begin stx)
-       (syntax-parse stx
-         [(_ defns:id e ...) ; the `defns` identifier is added by the reader
-          #`(#%module-begin
-             (define defns
-               (filter (redex-match? grammar-id defn-pattern) (term (e ...))))
-             (run reductions (prog e ...)))]))
+    (define-syntax (module-begin stx)
+      (syntax-parse stx
+        [(_ defns:id e (... ...)) ; the `defns` identifier is added by the reader
+         #`(#%module-begin
+            (define defns
+              (filter (redex-match? grammar-id defn-pattern) (term (e (... ...)))))
+            (run-all reductions ...  (prog e (... ...))))]))
 
-     ;; ---------------------------------------------------------------------------------------------------
-     ;; reader
+    ;; ---------------------------------------------------------------------------------------------------
+    ;; reader
 
-     (module reader syntax/module-reader
-       lang-module-name
-       
-       #:read read
-       #:read-syntax read-syntax
-       #:wrapper1 (λ (x) (cons 'definitions (x))) ; include `definitions` as if in the original
-       
-       (require racket)))))
-
-
-(define-syntax-rule (define-language-set
-                      #:module-name lang-module-name
-                      #:reductions1 reductions1
-                      #:reductions2 reductions2
-                      #:reductions3 reductions3
-                      #:grammar grammar-id
-                      #:defn-pattern defn-pattern)
-  (...
-   (begin
-     
-     (provide
-      (rename-out
-       [top-interaction #%top-interaction]
-       [module-begin    #%module-begin]))
-
-     ;; ---------------------------------------------------------------------------------------------------
-     ;; dependencies
-
-     (require RacketSchool/private/mystery
-              (for-syntax racket/base
-                          syntax/parse)
-              redex/reduction-semantics)
-
-     ;; ---------------------------------------------------------------------------------------------------
-     ;; implementation
-
-     (define-syntax (top-interaction stx)
-       (syntax-case stx ()
-         [(_ . e)
-          #`(#%top-interaction . (run-all reductions1 reductions2 reduction3 (prog ,@definitions e)))]))
-
-     (define-syntax (module-begin stx)
-       (syntax-parse stx
-         [(_ defns:id e ...) ; the `defns` identifier is added by the reader
-          #`(#%module-begin
-             (define defns
-               (filter (redex-match? grammar-id defn-pattern) (term (e ...))))
-             (run-all reductions1 reductions2 reductions3 (prog e ...)))]))
-
-     ;; ---------------------------------------------------------------------------------------------------
-     ;; reader
-
-     (module reader syntax/module-reader
-       lang-module-name
-       
-       #:read read
-       #:read-syntax read-syntax
-       #:wrapper1 (λ (x) (cons 'definitions (x))) ; include `definitions` as if in the original
-       
-       (require racket)))))
+    (module reader syntax/module-reader
+      lang-module-name
+      
+      #:read read
+      #:read-syntax read-syntax
+      #:wrapper1 (λ (x) (cons 'definitions (x))) ; include `definitions` as if in the original
+      
+      (require racket))))
